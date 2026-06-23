2f35c9f074f03aaf06c670b27d493db43a0edc42
sushanth0903-CODE <sushanth0903@gmail.com>
2026-06-23 23:47:57 +0530

    updated my text

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello GitHub! I just built my first official file!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
