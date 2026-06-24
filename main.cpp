7c07b65daf538f684ca3197eb987002494068cf3
sushanth0903-CODE <sushanth0903@gmail.com>
2026-06-24 11:49:04 +0530

    my calculator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    double num1, num2;
+    char op;
+
+    cout << "=== MY CALCULATOR ===" << endl;
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Result: ";
+    if (op == '+') cout << num1 + num2;
+    else if (op == '-') cout << num1 - num2;
+    else if (op == '*') cout << num1 * num2;
+    else if (op == '/') cout << num1 / num2;
+    else cout << "Invalid operator!";
+    
+    cout << endl;
     return 0;
 }
